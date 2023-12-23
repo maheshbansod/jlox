d2f1c8c781168a4c27b434386a5b2ec8f29a589f
Mahesh Bansod <coolkevin011@gmail.com>
2023-12-24 00:46:43 +0530

    throw division by zero error

--- com/light/jlox/Interpreter.java
+++ com/light/jlox/Interpreter.java
@@ -39,6 +39,9 @@ class Interpreter implements Expr.Visitor<Object> {
                 return (double)left - (double)right;
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+                if ((double)right == 0) {
+                    throw new RuntimeError(expr.operator, "Division by zero error");
+                }
                 return (double)left / (double)right;
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
